6e25a10f82e3eaefd0c03834b10d11c23711aca2
Knarkzel <knarkzel@gmail.com>
2021-12-21 21:36:13 +0100

    use match for stream

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,9 @@ use std::{
     str::from_utf8,
 };
 
-const INDEX: &str = r#"
-<h1>Todos</h1>
-<form action="/create" method="post">
-  <label for="todo">Add todo note:</label>
-  <input type="text" id="todo" name="todo">
-  <input type="submit" value="Submit">
-</form>
-"#;
-
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-fn respond(message: &str) -> Vec<u8> {
+fn respond(message: String) -> Vec<u8> {
     format!(
         "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
         message.len(),
@@ -25,17 +16,25 @@ fn respond(message: &str) -> Vec<u8> {
     .to_vec()
 }
 
+struct Todo {
+    message: String,
+}
+
+const INDEX: &str = r#"
+<h1>Todos</h1>
+<form action="/create" method="post">
+  <label for="todo">Add todo note: </label>
+  <input type="text" id="todo" name="todo">
+  <input type="submit" value="Submit">
+</form>
+"#;
+
 fn index(todos: &[Todo]) -> String {
     let messages = todos
         .into_iter()
-        .filter(|it| it.message.len() > 0)
         .map(|it| format!("<li>{}</li>", it.message))
         .collect::<String>();
-    format!("{}{}", INDEX, messages)
-}
-
-struct Todo {
-    message: String,
+    format!("{}<ul>{}</ul>", INDEX, messages)
 }
 
 fn main() -> Result<()> {
@@ -43,35 +42,43 @@ fn main() -> Result<()> {
     println!("Listening to 0.0.0.0:8000");
     let listener = TcpListener::bind("0.0.0.0:8000")?;
 
-    for mut stream in listener.incoming().flatten() {
-        // Boiler plate
-        let mut buffer = [0; 1024];
-        stream.read(&mut buffer)?;
-        let mut raw = from_utf8(&buffer)?;
-        if let Some(index) = raw.find("\u{0}") {
-            raw = &raw[..index];
-        }
-        let request = raw.split_whitespace().take(2).collect::<String>();
-
-        // Handle paths
-        match request.as_str() {
-            "GET/" => {
-                stream.write(&respond(&index(&todos)))?;
-            }
-            "POST/create" => {
-                if let Some(header) = raw.split("\r\n\r\n").skip(1).next() {
-                    if let Some(message) = header.split("=").skip(1).next().map(str::to_string) {
-                        todos.push(Todo { message });
-                    }
-                    stream.write(&respond(&index(&todos)))?;
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                // Boiler plate
+                let mut buffer = [0; 1024];
+                stream.read(&mut buffer)?;
+                let mut raw = from_utf8(&buffer)?;
+                if let Some(index) = raw.find("\u{0}") {
+                    raw = &raw[..index];
                 }
-            }
-            unknown => {
-                stream.write(&respond(&format!("{:?}\r\nData: {:?}", unknown, raw)))?;
-            }
-        }
-        stream.flush()?;
-    }
+                let request = raw.split_whitespace().take(2).collect::<String>();
 
+                // Handle requests
+                match request.as_str() {
+                    "GET/" => {
+                        stream.write(&respond(index(&todos)))?;
+                    }
+                    "POST/create" => {
+                        if let Some(header) = raw.split("\r\n\r\n").skip(1).next() {
+                            if let Some(message) =
+                                header.split("=").skip(1).next().map(str::to_string)
+                            {
+                                if message.len() > 0 {
+                                    todos.push(Todo { message });
+                                }
+                            }
+                            stream.write(&respond(index(&todos)))?;
+                        }
+                    }
+                    unknown => {
+                        stream.write(&respond(format!("{:?}\r\nData: {:?}", unknown, raw)))?;
+                    }
+                }
+                stream.flush()?;
+            }
+            Err(e) => println!("Error occured: {:?}", e),
+        }
+    }
     Ok(())
 }
